247b05556bb74c274afb1e08629c818d1c843763
rickyjena <ricky.jena@gmail.com>
2021-11-02 22:37:48 -0700

    Finished left side of topbar along with contact info

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,23 @@
 import "./topbar.scss"
+import {Person,Mail} from "@mui/icons-material";
 
 export default function Topbar() {
     return (
         <div className="topbar">
-
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">Home.</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+1(562)-458-5525</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>ricky.jena@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right"></div>
+            </div>
         </div>
     )
 }
